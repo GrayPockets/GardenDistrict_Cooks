963dfea10fa3fcdd9705d270b8ad138220d5bda1
Matthew J. H. Peronto <matthew.peronto@gmail.com>
2024-03-28 18:44:31 -0700

    Initial Commit
    * Gardens start with a Pavillion for games begining in the Renaissance Era or later

---
+++ README.md
@@ -0,0 +1,11 @@
+# GrayPockets - Leugi's Garden District - Cooks
+
+## Description
+
+This mod cooks several items in "Leugi's Garden District"
+
+* Gardens start with a Pavillion for games begining in the Renaissance Era or later
+
+## Requirements
+
+* Leugi's Garden District
---
+++ SQL/Game/GardenDIstrictCooks.sql
@@ -0,0 +1,6 @@
+-- Gardens start with a Pavillion for games begining in the Renaissance Era or later
+
+INSERT INTO "StartingBuildings"
+	(	"Building",					"Era",				"District",				"MinorOnly",	"MinDifficulty"	)
+VALUES
+	(	'BUILDING_LEU_PAVILLION',	'ERA_RENAISSANCE', 	'DISTRICT_LEU_GARDEN',	0,				NULL			);
